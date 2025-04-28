a548f5139d974337ba18e1a644a8e8d0f87124a2
Mark Quinn <mobious_99@yahoo.com>
2025-04-27 20:00:13 -0400

    Fixing mkdocs template_python typo

--- bump_version.py
+++ bump_version.py
@@ -1,50 +1,28 @@
-"""Script to bump the version of a Python package.
-
-This script increments the version of a Python package by modifying the version
-in the package's __init__.py file. The version is specified according to the
-Semantic Versioning 2.0.0 specification:
-https://semver.org/spec/v2.0.0.html
-
-The script takes two arguments: the current version as a string and the part of
-the version to increment ('major', 'minor', or 'patch'). The script will then
-increment the specified part of the version and return the new version string.
-
-"""
-
 import sys
 
 from packaging.version import Version
 
 
 def next_version(current: str, part: str) -> str:
-    """Increment a version.
+    """
+    Increment a version.
 
     Args:
-    ----
-        current (str): The current version string.
-        part (str): The part of the version to increment ('major', 'minor', or 'patch').
+        current: The current version string.
+        part: The part of the version to increment ('major', 'minor', or 'patch').
 
     Returns:
-    -------
-        str: A new version string with the specified part incremented.
-
-    Raises:
-    ------
-        ValueError: If the part is not 'major', 'minor', or 'patch'.
-
+        A new version string with the specified part incremented.
     """
     v = Version(current)
     if part == "major":
-        # Increment the major version and reset the minor and patch versions.
         return f"{v.major + 1}.0.0"
     elif part == "minor":
-        # Increment the minor version and reset the patch version.
         return f"{v.major}.{v.minor + 1}.0"
     elif part == "patch":
-        # Increment the patch version.
         return f"{v.major}.{v.minor}.{v.micro + 1}"
     else:
-        raise ValueError(f"Error: Specify 'major', 'minor', or 'patch', not {part}.")
+        raise ValueError("Specify 'major', 'minor', or 'patch'.")
 
 
 if __name__ == "__main__":
